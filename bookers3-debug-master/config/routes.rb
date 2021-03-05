82653212227740314a1ff926f39b7e87340d6401
Leica913 <toruneko1996913@gmail.com>
2021-03-05 18:25:05 +0000

    [Finish] follow-favorite

--- bookers3-debug-master/config/routes.rb
+++ bookers3-debug-master/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 
   root to: 'homes#top'
 
